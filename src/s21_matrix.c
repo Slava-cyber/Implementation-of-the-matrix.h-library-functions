1c7c593a3e28d7ef6a92fb7eae1c8463a1193513
Dougie Zoraida <dzoraida@si-b5.nsk.21-school.ru>
2022-01-23 20:20:35 +0700

    determinant

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -26,32 +26,60 @@ matrix_t s21_mult_matrix(matrix_t *A, matrix_t *B);
 matrix_t s21_transpose(matrix_t *A);
 matrix_t s21_calc_complements(matrix_t *A);
 double s21_determinant(matrix_t *A);
-matrix_t mini_matrix(int n, int m, int *A);
-double get_minor(matrix_t *A);
+    // вспомогательные функции
+matrix_t mini_matrix(int n, int m, matrix_t *A);
+double get_minor(int n, int m, matrix_t *A);
+int type_matrix(matrix_t *A);
+
 
 int main() {
     matrix_t matrix1, matrix2, matrix3;
-    int n = 3, m = 2;
-    matrix1 = s21_create_matrix(n, m);
-    matrix2 = s21_create_matrix(m, n);
+    int n = 4, m = 2;
+    matrix1 = s21_create_matrix(n, n);
+    //matrix2 = s21_create_matrix(m, n);
     matrix1.matrix[0][0] = 1;
+    matrix1.matrix[0][1] = 21;
+    matrix1.matrix[0][2] = 32;
+    matrix1.matrix[0][3] = 47;
     matrix1.matrix[1][0] = 2;
-    matrix1.matrix[2][0] = 3;
-    matrix1.matrix[0][1] = 4;
-    matrix1.matrix[1][1] = 5;
-    matrix1.matrix[2][1] = 6;
+    matrix1.matrix[1][1] = 23;
+    matrix1.matrix[1][2] = 3;
+    matrix1.matrix[1][3] = 12;
+    matrix1.matrix[2][0] = 5;
+    matrix1.matrix[2][1] = 54;
+    matrix1.matrix[2][2] = 4;
+    matrix1.matrix[2][3] = 32;
+    matrix1.matrix[3][0] = 89;
+    matrix1.matrix[3][1] = 7;
+    matrix1.matrix[3][2] = 61;
+    matrix1.matrix[3][3] = 5;
 
-    matrix2.matrix[0][0] = 1;
-    matrix2.matrix[0][1] = -1;
-    matrix2.matrix[0][2] = 1;
-    matrix2.matrix[1][0] = 2;
-    matrix2.matrix[1][1] = 3;
-    matrix2.matrix[1][2] = 4;
+    // matrix1.matrix[0][0] = 1;
+    // matrix1.matrix[0][1] = 2;
+    // matrix1.matrix[0][2] = 3;
+    // matrix1.matrix[1][0] = 0;
+    // matrix1.matrix[0][0] = 4;
+    // matrix1.matrix[0][1] = -4;
+    // matrix1.matrix[1][0] = 2;
+    // matrix1.matrix[1][1] = 3;
 
+    // matrix2.matrix[0][0] = 1;
+    // matrix2.matrix[0][1] = -1;
+    // matrix2.matrix[0][2] = 1;
+    // matrix2.matrix[1][0] = 2;
+    // matrix2.matrix[1][1] = 3;
+    // matrix2.matrix[1][2] = 4;
+    double deter;
     //matrix3 = s21_mult_matrix(&matrix1, &matrix2);
-    matrix3 = s21_transpose(&matrix1);
-    //printf("eq:%d\n", s21_eq_matrix(&matrix1, &matrix2));
-    for (int i = 0; i < m; i++) {
+    deter = s21_determinant(&matrix1);
+    printf("deter:%f\n", deter);
+    matrix3 = s21_calc_complements(&matrix1);
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            printf("%f ", matrix1.matrix[i][j]); 
+        printf("\n");
+    }
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
             printf("%f ", matrix3.matrix[i][j]); 
         printf("\n");
@@ -109,11 +137,12 @@ matrix_t s21_sum_matrix(matrix_t *A, matrix_t *B) {
         result.matrix_type = INCORRECT_MATRIX;
     } else {
         result = s21_create_matrix(A->rows, A->columns);
-        result.matrix_type = CORRECT_MATRIX;
+        //result.matrix_type = CORRECT_MATRIX;
         for (int i = 0; i < A->rows; i++) {
             for (int j = 0; j < A-> columns; j++)
                 result.matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
         }
+        type_matrix(&result);
     }
     return result;
 }
@@ -124,11 +153,12 @@ matrix_t s21_sub_matrix(matrix_t *A, matrix_t *B) {
         result.matrix_type = INCORRECT_MATRIX;
     } else {
         result = s21_create_matrix(A->rows, A->columns);
-        result.matrix_type = CORRECT_MATRIX;
+        //result.matrix_type = CORRECT_MATRIX;
         for (int i = 0; i < A->rows; i++) {
             for (int j = 0; j < A-> columns; j++)
                 result.matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
         }
+        type_matrix(&result);
     }
     return result;
 }
@@ -136,11 +166,12 @@ matrix_t s21_sub_matrix(matrix_t *A, matrix_t *B) {
 matrix_t s21_mult_number(matrix_t *A, double number) {
     matrix_t result;
     result = s21_create_matrix(A->rows, A->columns);
-    result.matrix_type = CORRECT_MATRIX;
+    //result.matrix_type = CORRECT_MATRIX;
     for (int i = 0; i < A->rows; i++) {
         for (int j = 0; j < A-> columns; j++)
             result.matrix[i][j] = A->matrix[i][j] * number;
     }
+    type_matrix(&result);
     return result;
 }
 
@@ -150,7 +181,7 @@ matrix_t s21_mult_matrix(matrix_t *A, matrix_t *B) {
         result.matrix_type = INCORRECT_MATRIX;
     } else {
         result = s21_create_matrix(A->rows, B->columns);
-        result.matrix_type = CORRECT_MATRIX;
+        //result.matrix_type = CORRECT_MATRIX;
         for (int i = 0; i < A->rows; i++) {
             for (int j = 0; j < B->columns; j++) {
                 for (int k = 0; k < A->columns; k++) {
@@ -158,6 +189,7 @@ matrix_t s21_mult_matrix(matrix_t *A, matrix_t *B) {
                 }
             }
         }
+        type_matrix(&result);
     }
     return result;
 }
@@ -165,11 +197,12 @@ matrix_t s21_mult_matrix(matrix_t *A, matrix_t *B) {
 matrix_t s21_transpose(matrix_t *A) {
     matrix_t result;
     result = s21_create_matrix(A->columns, A->rows);
-    result.matrix_type = CORRECT_MATRIX;
+    //result.matrix_type = CORRECT_MATRIX;
     for (int i = 0; i < A->columns; i++) {
         for (int j = 0; j < A->rows; j++)
             result.matrix[i][j] = A->matrix[j][i];
     }
+    type_matrix(&result);
     return result;
 }
 
@@ -180,23 +213,28 @@ matrix_t s21_calc_complements(matrix_t *A) {
     if (A->rows != A->columns) {
         result.matrix_type = INCORRECT_MATRIX;
     } else {
+        result = s21_create_matrix(A->rows, A->rows);
         if (A->rows == 2) {
-            minor = A->matrix[0][0] * A->matrix[1][1] - A->matrix[0][1] * A->matrix[1][0];
+            result.matrix[0][0] = A->matrix[1][1];
+            result.matrix[1][1] = A->matrix[0][0];
+            result.matrix[0][1] = A->matrix[1][0] * (-1);
+            result.matrix[1][0] = A->matrix[0][1] * (-1);
         } else {
             for (int i = 0; i < A->rows; i++) {
                 for (int j = 0; j < A->rows; j++) {
-                    result.matrix[i][j] = get_minor(i, j, *A) * pow(-1, i + j);
+                    result.matrix[i][j] = get_minor(i, j, A) * pow(-1, i + j);
                 }
             }
         }
+        type_matrix(&result);
     }
     return result;
 }
 
-double get_minor(matrix_t *A) {
+double get_minor(int n, int m, matrix_t *A) {
     double determinant;
     matrix_t buffer;
-    buffer = mini_matrix(n, m, &A);
+    buffer = mini_matrix(n, m, A);
     determinant = s21_determinant(&buffer);
     return determinant;
 }
@@ -210,25 +248,48 @@ double s21_determinant(matrix_t *A) {
         result = A->matrix[0][0] * A->matrix[1][1] - A->matrix[0][1] * A->matrix[1][0];
     } else {
         for (int i = 0; i < A->rows; i++) {
-           buffer = mini_matrix(i, 0, A);
-           result = pow(-1, i + j) * s21_determinant(&buffer);
+            buffer = mini_matrix(i, 0, A);
+            result += pow(-1, i + 2) * A->matrix[i][0] *s21_determinant(&buffer);
         }
     }
     return result;
 }
 
-matrix_t mini_matrix(int n, int m, int *A) {
+matrix_t mini_matrix(int n, int m, matrix_t *A) {
     matrix_t result;
     result = s21_create_matrix(A->rows - 1, A->rows - 1);
     int l = 0, k = 0;
     for (int i = 0; i < A->rows; i++) {
-        for (int j = 0; j < A->rows; j++) {
-            if (j != m) {
-                buffer.matrix[k][l] = A->matrix[i][j];
-                l += 1;
+        if (i != n) {
+            for (int j = 0; j < A->rows; j++) {
+                if (j != m) {
+                    result.matrix[k][l] = A->matrix[i][j];
+                    l += 1;
+                }
             }
+            k += 1;
+            l = 0;
         }
-        if (i != n) k += 1;
     }
     return result;
+}
+
+int type_matrix(matrix_t *A) {
+    int zero = 0, one = 0;
+    for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+            if (A->matrix[i][j] == 0)
+                zero += 1;
+            if (A->matrix[i][j] == 1 && i == j)
+                one += 1;            
+        }
+    }
+    if (zero == A->rows * A->columns) {
+        A->matrix_type = ZERO_MATRIX;
+    } else if (one == A->rows && A->rows == A->columns) {
+        A->matrix_type = IDENTITY_MATRIX;
+    } else {
+        A->matrix_type = CORRECT_MATRIX;
+    }
+    return 1;
 }
